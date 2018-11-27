848428f295edbb37f8a0c189ef6834fe182076e3
Juan Manuel Lopez <git.nrbye@slmail.me>
2018-11-27 18:25:15 -0300

    Create temporal Dummy unit test

---
+++ tests/unit/DummyCest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace VendorName\PackageName\Tests;
+
+class DummyCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
